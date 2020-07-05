5d2d8f1c69886f08b2f4581c7b75aa13bdcf2465
dropdead666 <therealloathing@gmail.com>
2020-07-05 06:23:40 +0300

    asd

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,7 +1,7 @@
 var mymap = L.map('map').setView([0, 0], 13);
 markerUser =  L.marker([0, 0]).addTo(mymap);
 getLocation();
-centerMap();
+
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
